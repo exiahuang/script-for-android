98baf0467f8f186a6dc47b2ccd959ba2987df751
Exia.huangxy <exia.huang@outlook.com>
2020-02-24 18:27:58 +0900

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# download classify
+
+```sh
+# download
+curl -O -L -s https://raw.githubusercontent.com/exiahuang/script-for-android/master/classify.sh
+
+# usage
+./classify.sh $source_path $dest_path
+
+# example
+./classify.sh /sdcard/download /sdcard/download
+./classify.sh /sdcard/tencent/MicroMsg/Download /sdcard/download
+
+```
---
+++ classify.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+classify() {
+    if [ -z "$1" ] || [ -z "$2" ]
+    then
+        echo "No argument supplied"
+        return
+    fi
+    echo "source_path : $1"
+    echo "dest_path : $2"
+
+    whoami
+    source_path=$1
+    dest_path=$2
+
+    echo "make directory"
+    mkdir -p "$dest_path/software"
+    mkdir -p "$dest_path/ebook"
+    mkdir -p "$dest_path/pdf"
+    mkdir -p "$dest_path/doc"
+    mkdir -p "$dest_path/xls"
+    mkdir -p "$dest_path/pic"
+    mkdir -p "$dest_path/zip"
+    cd "$source_path"
+    pwd
+    mv *.zip *.tar *.gz "$dest_path/zip/"
+    mv *.msi *.exe *.apk "$dest_path/software/"
+    mv *.epub *.azw *.mobi "$dest_path/ebook/"
+    mv *.pdf "$dest_path/pdf/"
+    mv *.txt *.pdf *.doc *.docx *.ppt *.pptx "$dest_path/doc/"
+    mv *.xla *.xls *.xlsx *.csv *.CSV *.xlsm "$dest_path/xls/"
+    mv *.jpg *.png "$dest_path/pic/"
+}
+
+classify $1 $2
