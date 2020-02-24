26bee2c4ff20c9653f7e40376afa54e59df392ab
Exia.huangxy <exia.huang@outlook.com>
2020-02-24 18:44:32 +0900

    Add files via upload

--- classify.sh
+++ classify.sh
@@ -21,15 +21,17 @@ classify() {
     mkdir -p "$dest_path/xls"
     mkdir -p "$dest_path/pic"
     mkdir -p "$dest_path/zip"
+    mkdir -p "$dest_path/video"
     cd "$source_path"
     pwd
-    mv *.zip *.tar *.gz "$dest_path/zip/"
+    mv *.zip *.rar *.tar *.gz "$dest_path/zip/"
     mv *.msi *.exe *.apk "$dest_path/software/"
     mv *.epub *.azw *.mobi "$dest_path/ebook/"
     mv *.pdf "$dest_path/pdf/"
     mv *.txt *.pdf *.doc *.docx *.ppt *.pptx "$dest_path/doc/"
     mv *.xla *.xls *.xlsx *.csv *.CSV *.xlsm "$dest_path/xls/"
     mv *.jpg *.jpeg *.png "$dest_path/pic/"
+    mv *.mp4 *.avi *.webm *.mkv "$dest_path/video/"
 }
 
 classify $1 $2
